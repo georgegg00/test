073dc174b3847373437034a649498e2969a97ae4
Student <Student@5-USR-115P-06.std5.local>
2024-02-09 10:37:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Comparison
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 число:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("2 число:");
+            Console.ForegroundColor = ConsoleColor.Green;
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {   
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a > b");
+            }
+            if (a < b)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a < b");
+            }
+            else _= (a == b);
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(a == b);
+            }
+            Console.ReadKey();
+        }
+    }
+}
